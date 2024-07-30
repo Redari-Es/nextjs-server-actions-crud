c3459896e27e9fff2796b335898ccc64b33ae659
Redari-Es <905422030@qq.com>
2024-07-30 11:05:47 +0800

    update

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,11 +1,11 @@
 import { TaskForm } from "./task-form"
 
 function NewPage() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-       <TaskForm/> 
-    </div>
-  )
+	return (
+		<div className="flex items-center justify-center h-screen">
+			<TaskForm />
+		</div>
+	)
 }
 
 export default NewPage
