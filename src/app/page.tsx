f7fac518c72cf4eaa60dc47391d42f2f24a2a67a
Redari-Es <905422030@qq.com>
2024-08-30 22:02:52 +0800

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,19 @@ import prisma from '@/lib/prisma'
 import React from 'react'
 
 async function HomePage() {
-  const tasks=await prisma.task.findMany()
-  console.log(tasks)
-  
+	const tasks = await prisma.task.findMany()
+	console.log(tasks)
 
-  return (
-    <div className='grid grid-cols-3 gap-4'>
-      {tasks.map(task=>(
-<TaskCard task={task} key={task.id}/>
 
-      ))}
-      
-    </div>
-  )
+	return (
+		<div className='grid grid-cols-3 gap-4'>
+			{tasks.map(task => (
+				<TaskCard task={task} key={task.id} />
+
+			))}
+
+		</div>
+	)
 }
 
 export default HomePage
